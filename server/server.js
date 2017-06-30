ea34b391e0108432b95fcf62a32863b8e522b643
Jay <i9055378@googlemail.com>
2017-06-30 11:35:23 +0100

    efs

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ var path        = require('path');
 const MongoClient = require('mongodb').MongoClient;
 
 app.use(express.static(path.resolve(__dirname, '../react', 'build')));
-/*app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../../../', 'build', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'));
 });
-*/
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
