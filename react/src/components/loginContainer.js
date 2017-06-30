e6a378a16419527a1c084872848946c284e5419e
Jay <i9055378@googlemail.com>
2017-06-30 12:09:52 +0100

    Changed login api URL for test

--- react/src/components/loginContainer.js
+++ react/src/components/loginContainer.js
@@ -25,7 +25,7 @@ class LoginContainer extends Component {
       'username': this.state.username,
       'password': this.state.password
     }
-    axios.post(`http://localhost:3010/api/login`, names)
+    axios.post(`/api/login`, names)
       .then( response => {
         if(response.data.auth){
 
