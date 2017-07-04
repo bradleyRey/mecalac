fef0f7df0706d98d546b5d76013b4c0e35e800e5
Jay <i9055378@googlemail.com>
2017-07-04 10:05:49 +0100

    Changed values for Heroku - need to fix this by making environment variables

--- react/src/api/leads.js
+++ react/src/api/leads.js
@@ -3,27 +3,27 @@ import axios from 'axios';
 class LeadsApi {
 
   static getLeads(dealerid, callback){
-    axios.post(`http://localhost:3010/api/getLeadsById`, {dealerid: dealerid})
+    axios.post(`/api/getLeadsById`, {dealerid: dealerid})
       .then( response => {
         return callback(response)
       })
   }
   static getLeadById(leadid, callback){
-    axios.post(`http://localhost:3010/api/getSingleLeadById`, {leadid: leadid})
+    axios.post(`/api/getSingleLeadById`, {leadid: leadid})
       .then( response => {
         return callback(response)
       })
   }
 
   static submitLead(leadid, updateType, updateData, callback){
-    axios.post(`http://localhost:3010/api/updateLead`, {leadid: leadid, updateData: updateData, updateType: updateType})
+    axios.post(`/api/updateLead`, {leadid: leadid, updateData: updateData, updateType: updateType})
       .then( response => {
         return callback(response)
       })
   }
 
   static viewDealers(callback){
-    axios.post('http://localhost:3010/api/getDealerNames')
+    axios.post('/api/getDealerNames')
       .then( response => {
         return callback(response)
       })
